b3f9c729100ffc426e552ac2d4861156abf88f21
ta2k <tombox2020@gmail.com>
2019-11-13 19:52:49 +0100

    first working version

--- main.js
+++ main.js
@@ -47,11 +47,12 @@ class WeishauptWem extends utils.Adapter {
 		this.login().then(() => {
 			this.log.debug("Login successful");
 			this.setState("info.connection", true, true);
-			// this.getHomesStatus().then(() => {});
-			// this.updateInterval = setInterval(() => {
-			// 	this.getHomesStatus();
-			// }, this.config.interval * 60 * 1000)
-
+			this.switchFachmann().then(() => {
+				this.getStatus().then(() => {});
+				this.updateInterval = setInterval(() => {
+					this.getStatus();
+				}, this.config.interval * 60 * 1000)
+			})
 		});
 
 
@@ -89,8 +90,9 @@ class WeishauptWem extends utils.Adapter {
 							form[formElement.name] = formElement.value;
 						}
 					}
-					form["ctl00_content_tbxUserName"] = this.config.user;
+					form["ctl00$content$tbxUserName"] = this.config.user;
 					form["ctl00$content$tbxPassword"] = this.config.password;
+					form["ctl00$content$btnLogin"] = "Anmelden";
 					request.post({
 						url: "https://www.wemportal.com/Web/Login.aspx",
 						headers: {
@@ -101,16 +103,19 @@ class WeishauptWem extends utils.Adapter {
 							"Content-Type": "application/x-www-form-urlencoded",
 						},
 						form: form,
+						gzip: true,
 						jar: this.jar,
-						followAllRedirects: true,
+						followAllRedirects: false,
 					}, (err, resp, body) => {
 						if (err) {
 							this.log.error(err);
 							reject();
 						} else {
-							
 							try {
 								this.log.debug(body);
+								if (body.indexOf('Object moved to <a href="/Web/Default.aspx"') !== -1) {
+									resolve()
+								}
 							} catch (error) {
 								this.log.error(error);
 								reject();
@@ -126,55 +131,150 @@ class WeishauptWem extends utils.Adapter {
 			});
 		});
 	}
-
-	refreshToken() {
+	switchFachmann() {
 		return new Promise((resolve, reject) => {
-			this.log.debug("refreshToken");
-
-			request.post({
-				url: "",
+			request.get({
+				url: "https://www.wemportal.com/Web/Default.aspx",
 				headers: {
-					
+					'Accept-Language': 'en-US,en;q=0.9,de-DE;q=0.8,de;q=0.7,lb;q=0.6',
+					'Accept-Encoding': 'gzip, deflate, br',
+					'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.29 Safari/537.36',
+					"Accept": "*/*"
 				},
-				form: {
-				},
+				gzip: true,
+				jar: this.jar,
 				followAllRedirects: true
 			}, (err, resp, body) => {
 				if (err) {
 					this.log.error(err);
 					reject();
 				}
+
 				try {
-				
+					const dom = new JSDOM(body);
+					let form = {};
+					for (const formElement of dom.window.document.querySelectorAll("input")) {
+						if (formElement.type === "hidden") {
+							//form += formElement.name + "=" + formElement.value + "&";
+							form[formElement.name] = formElement.value;
+						}
+					}
+					form["__EVENTTARGET"] = "ctl00$SubMenuControl1$subMenu";
+					form["__EVENTARGUMENT"] = "3";
+					form["ctl00_SubMenuControl1_subMenu_ClientState"] = '{"logEntries":[{"Type":3},{"Type":1,"Index":"0","Data":{"text":"Übersicht","value":"110"}},{"Type":1,"Index":"1","Data":{"text":"Anlage:","value":""}},{"Type":1,"Index":"2","Data":{"text":"Benutzer","value":"222"}},{"Type":1,"Index":"3","Data":{"text":"Fachmann","value":"223","selected":true}},{"Type":1,"Index":"4","Data":{"text":"Statistik","value":"225"}},{"Type":1,"Index":"5","Data":{"text":"Datenlogger","value":"224"}}],"selectedItemIndex":"3"}'
+					request.post({
+						url: "https://www.wemportal.com/Web/Default.aspx",
+						headers: {
+							'Accept-Language': 'en-US,en;q=0.9,de-DE;q=0.8,de;q=0.7,lb;q=0.6',
+							'Accept-Encoding': 'gzip, deflate, br',
+							'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.29 Safari/537.36',
+							"Accept": "*/*",
+							"Content-Type": "application/x-www-form-urlencoded",
+						},
+						form: form,
+						gzip: true,
+						jar: this.jar,
+						followAllRedirects: false,
+					}, (err, resp, body) => {
+						if (err) {
+							this.log.error(err);
+							reject();
+						} else {
+							try {
+								this.log.debug(body);
+								if (body.indexOf('Object moved to <a href="https://www.wemportal.com/Web/Default.aspx"') !== -1) {
+									resolve()
+								}
+							} catch (error) {
+								this.log.error(error);
+								reject();
+							}
+						}
+
+					});
 
 				} catch (error) {
 					this.log.error(error);
+					this.log.error(error.stack);
 					reject();
 				}
 			});
 		});
 	}
 
-	getHomesStatus() {
+	getStatus() {
 		return new Promise((resolve, reject) => {
 			this.log.debug("getHomesStatus");
-			request.post({
-				url: "",
+			request.get({
+				url: "https://www.wemportal.com/Web/Default.aspx",
 				headers: {
-				
+					'Accept-Language': 'en-US,en;q=0.9,de-DE;q=0.8,de;q=0.7,lb;q=0.6',
+					'Accept-Encoding': 'gzip, deflate, br',
+					'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.29 Safari/537.36',
+					"Accept": "*/*",
+
 				},
-				json: true,
-				followAllRedirects: true
+				gzip: true,
+				jar: this.jar,
+				followAllRedirects: true,
+
 			}, (err, resp, body) => {
 				if (err) {
 					this.log.error(err);
 					reject();
 				}
 				try {
-					
+					const dom = new JSDOM(body);
+					let form = {};
+					const deviceInfo = dom.window.document.querySelector(".DeviceInfo").textContent;
+					this.setObjectNotExists(deviceInfo, {
+						type: "device",
+						common: {
+							name: deviceInfo,
+							role: "indicator",
+							type: "mixed",
+							write: false,
+							read: true
+						},
+						native: {}
+					});
+
+					const status = dom.window.document.querySelector("#ctl00_DeviceContextControl1_DeviceStatusText").textContent;
+					this.setObjectNotExists(deviceInfo + ".status", {
+						type: "state",
+						common: {
+							name: "Status",
+							role: "indicator",
+							type: "mixed",
+							write: false,
+							read: true
+						},
+						native: {}
+					});
+					this.setState(deviceInfo + ".status", status, true);
+					for (const dataCell of dom.window.document.querySelectorAll(".simpleDataIconCell")) {
+						if (dataCell.nextSibling) {
+							const label = dataCell.nextElementSibling.textContent.trim().replace(/\./g, "_");
+							const value = dataCell.nextElementSibling.nextElementSibling.textContent.trim()
+							this.setObjectNotExists(deviceInfo + "." + label, {
+								type: "state",
+								common: {
+									name: label,
+									role: "indicator",
+									type: "mixed",
+									write: false,
+									read: true
+								},
+								native: {}
+							});
+							this.setState(deviceInfo + "." + label, value, true);
+
+						}
+					}
 					resolve();
 				} catch (error) {
 					this.log.error(error);
+					this.log.error(error.stack);
 					reject();
 				}
 			});
@@ -186,7 +286,7 @@ class WeishauptWem extends utils.Adapter {
 			request.post({
 				url: "",
 				headers: {
-				
+
 				},
 				body: body,
 				json: true,
@@ -215,7 +315,7 @@ class WeishauptWem extends utils.Adapter {
 		try {
 			this.log.info("cleaned everything up...");
 
-			clearInterval(this.refreshTokenInterval);
+			// clearInterval(this.refreshTokenInterval);
 			clearInterval(this.updateInterval);
 			callback();
 		} catch (e) {
