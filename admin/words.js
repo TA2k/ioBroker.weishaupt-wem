936be6d53beeca74060b0687abac00fbbb974567
ta2k <tombox2020@gmail.com>
2019-11-13 15:52:05 +0100

    intial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"weishaupt-wem adapter settings": {
+		"en": "Adapter settings for weishaupt-wem",
+		"de": "Adaptereinstellungen für weishaupt-wem",
+		"ru": "Настройки адаптера для weishaupt-wem",
+		"pt": "Configurações do adaptador para weishaupt-wem",
+		"nl": "Adapterinstellingen voor weishaupt-wem",
+		"fr": "Paramètres d'adaptateur pour weishaupt-wem",
+		"it": "Impostazioni dell'adattatore per weishaupt-wem",
+		"es": "Ajustes del adaptador para weishaupt-wem",
+		"pl": "Ustawienia adaptera dla weishaupt-wem",
+		"zh-cn": "weishaupt-wem的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "option2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
